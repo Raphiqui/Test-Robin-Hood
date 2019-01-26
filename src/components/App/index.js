98638afa162fab063c03aa444a3a258d5e697cb3
Norsse <raphael.pastre@gmail.com>
2019-01-26 12:03:00 +0100

    add Main shows itself/ css updates

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,18 +3,20 @@ import './App.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import Main from "../Main";
 
 
-class App extends Component {
+class Index extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>My Movies App</h1>
         </header>
+        <Main/>
       </div>
     );
   }
 }
 
-export default App;
+export default Index;
